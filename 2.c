a5c9e89d96f3697753c53c22ea67251598be7095
SeungJo <tmdwhdp@mju.ac.kr>
2021-11-06 12:48:14 +0900

    makefile, 2.c 업데이트

--- 2.c
+++ 2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "myProject.h"
 int printMyName2(void){
-    printf("2. Member2. is same person as member 1.(정태웅)\n");
-    return 1;
+    printf("2. my name is 예승조\n");
+    return 2;
 }
