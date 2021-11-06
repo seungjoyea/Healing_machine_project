e09721af7cef94b3dd4fafb982e01efcf9ea7871
Taewoong Jeong <jjttww0311@naver.com>
2021-11-06 12:10:53 +0900

    Member2
    조원2 이름

--- 2.c
+++ 2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "myProject.h"
 int printMyName2(void){
-    printf("2. Member2. is same person as member 1.(정태웅) \n");
+    printf("2. Member2. is same person as member 1.(정태웅)\n");
     return 1;
 }
