9addfb424c2406e1f5cfac4b4775a085e1e081eb
Taewoong Jeong <jjttww0311@naver.com>
2021-11-06 12:05:20 +0900

    Member1
    조원1 이름 작성

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "myProject.h"
+int printMyName1(void){
+    printf("1. my name is 정태웅\n");
+    return 1;
+}
---
+++ myProject.h
@@ -0,0 +1,7 @@
+#ifndef MY_PROJECT_H
+#define MY_PROJECT_H
+
+int printMyName1(void);
+int printMyName2(void);
+
+#endif
