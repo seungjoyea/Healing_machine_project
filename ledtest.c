b39a6c1c6c6ad00ac98396e7ea8cd4c835dc1acd
Taewoong Jeong <jjttww0311@naver.com>
2021-11-29 20:46:50 +0900

    ledtest.c
    ledtest.c

---
+++ ledtest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "led.h"
+
+int main(void)
+{
+
+
+ledLibInit();
+
+ledOnOff(2,1);
+ledLibExit();
+
+return 0;
+}
+
