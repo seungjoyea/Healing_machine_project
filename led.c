b5f01ccefb8166533f94c6e3aba9403657cb45c9
lion42 <74356913+lion42-cat@users.noreply.github.com>
2021-11-29 21:26:23 +0900

    update led.c
    add  ledAllBlink_4sec

--- led.c
+++ led.c
@@ -1,65 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <ctype.h>
-#include <sys/ipc.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+//#include “led.h”
 #include <fcntl.h>
-#include <unistd.h>
-#include "led.h"
 
 static unsigned int ledValue = 0;
-static int fd2 = 0;
-
-int ledOnOff (int ledNum, int onOff)
-{
-    int i=1;
-    i = i<<ledNum;
-    ledValue = ledValue& (~i);
-    if (onOff !=0) ledValue |= i;
-    write (fd2, &ledValue, 4);
-}
-
+static int fd = 0;
+static int num_4sec = 0;
 
 int ledLibInit(void)
 {
-    fd2 = open("/dev/periled", O_WRONLY);
-    if ( fd < 0 )
-    {
-    perror("driver (//dev//cnled) open error.\n");
-    return 1;
-    }
-    ledValue = 0;
+fd=open("/dev/periled", O_WRONLY);
+ledValue = 0;
+}
+
+int ledOnOff (int ledNum, int onOff)
+{
+int i=1;
+i = i<<ledNum;
+ledValue = ledValue& (~i);
+if (onOff !=0) ledValue |= i;
+write (fd, &ledValue, 4);
+}
+
+int ledAllBlink_4sec(void)
+{
+for(num_4sec=0;num_4sec<2;num_4sec++){
+ledValue = 0xFF;
+write (fd, &ledValue, 4);
+sleep(1);
+ledValue = 0x00;
+write (fd, &ledValue, 4);
+sleep(1);
+}
 }
 
 int ledLibExit(void)
 {
-    ledValue = 0;
-    //ledOnOff (0, 0);
-    close(fd2);
+ledValue = 0;
+ledOnOff (0, 0);
+close(fd);
 }
-
-/*
-//추가할 때 쓰기 헤더파일에도 추가
-int ledBlink(void)
-{
-    while(1){
-        write(fd, 0xFF,4);
-        delay(1000); //1초 대기
-        write(fd, 0x00,4);
-    }
- //카운터나 타임관련 전처리기 include하기
-}
-
-//이거 아니면
-int ledBlink(void)
-{
-    while(1){
-    ledOnOff (0xFF, 1);
-    delay(5000) ;
-    ledOnOff (0xFF, 0);   
-    }
-}
-*/
