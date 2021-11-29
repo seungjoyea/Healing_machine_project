2cd16a0d905ad3671467e7ee125df1e17dabede3
lion42 <74356913+lion42-cat@users.noreply.github.com>
2021-11-30 01:27:41 +0900

    Update buttontest.c

--- buttontest.c
+++ buttontest.c
@@ -6,14 +6,12 @@
 #include <fcntl.h> // for O_RDWR
 #include <sys/ioctl.h> // for ioctl
 
-
 int main(int argc, char *argv[])
 {
 int fp;
 char inputDevPath[200]={0,};
 
 fp = Initialize_Button();   //file open해서 fp 받음
-which_Button_did_you_push(); //버튼 뭐눌렸는지 출력
 
 close(fp);
 }
