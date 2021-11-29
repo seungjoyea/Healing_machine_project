e1fe5105c0b4b118949a54ad686c53b9fa004af5
lion42 <74356913+lion42-cat@users.noreply.github.com>
2021-11-29 22:33:30 +0900

    Update led.h

--- led.h
+++ led.h
@@ -1,23 +1,11 @@
 #ifndef _LED_H_
-
 #define _LED_H_
-
 int ledLibInit(void);
-
 int ledOnOff (int ledNum, int onOff);
-
 int test(void);
-
 int ledAllBlink_4sec(void);
-
 int AllLEDon_1s_twoLED_OFF_1s(void);
-
 int ledStatus (void);
-
 int ledLibExit(void);
-
-#define LED_DRIVER_NAME
-
-"/dev/periled”
-
+#define LED_DRIVER_NAME		"/dev/periled"
 #endif _LED_H_
