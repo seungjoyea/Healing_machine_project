c0d875a291e356e68760bfc2a1f6b8fd5c4705ad
Taewoong Jeong <jjttww0311@naver.com>
2021-11-30 21:32:47 +0900

    Update2 led.c
    헤더파일 전처리 추가

--- led.c
+++ led.c
@@ -1,5 +1,14 @@
-//#include “led.h”
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include "led.h"
 
 static unsigned int ledValue = 0;
 static int fd = 0;
