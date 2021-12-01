d873a0971b882cded50785b7aac269d23c5c4fe9
lion42 <74356913+lion42-cat@users.noreply.github.com>
2021-12-02 00:34:38 +0900

    Create colorled.h

---
+++ colorled.h
@@ -0,0 +1,16 @@
+
+int pwmActiveAll(void);
+int pwmInactiveAll(void);
+int pwmSetDuty(int dutyCycle, int pwmIndex);
+int pwmSetPeriod(int Period, int pwmIndex);
+int pwmSetPercent_RED(int percent);
+int pwmSetPercent_GREEN(int percent);
+int pwmSetPercent_BLUE(int percent);
+int pwmStartAll(void);
+int pwmLedInit(void);
+
+
+
+
+
+
