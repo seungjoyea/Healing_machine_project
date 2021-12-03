aa71cf29a35e3e699716295f389f0817fe99d795
lion42 <74356913+lion42-cat@users.noreply.github.com>
2021-12-03 22:17:39 +0900

    Create temperaturetest.c

---
+++ temperaturetest.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+char gbuf[10];
+
+
+int main(int argc, char **argv){
+ read_temperature();
+}
