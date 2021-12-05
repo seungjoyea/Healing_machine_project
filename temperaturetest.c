4c2eb37cd21da9613b151518911d63a4d7abd8d3
Taewoong Jeong <60162258@mju.a.ckr>
2021-12-05 18:36:13 +0900

    temperaturetest.c update2
    12.05

--- temperaturetest.c
+++ temperaturetest.c
@@ -9,9 +9,14 @@
 #include <sys/ioctl.h>
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
-char gbuf[10];
 
+#include "temperature.h"
 
-int main(int argc, char **argv){
- read_temperature();
+int main(void)
+{
+    double t;
+    t = read_temperature();
+    
+    printf("이것은 read_temperature 함수의 출력값(온도) : %lf \n", t);
+    return 0;
 }
