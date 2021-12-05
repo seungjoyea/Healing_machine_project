f1d1160d6f8137fa3828870780676d4f6d1b7d25
Taewoong Jeong <60162258@mju.a.ckr>
2021-12-05 18:35:45 +0900

    temperature.h update2
    12.05

--- temperature.h
+++ temperature.h
@@ -1,5 +1,8 @@
+#ifndef _TEMPERATURE_H_
+#define _TEMPERATURE_H_
 
-int spi_init(char filename[40]);
+int spi_init(void);
 char * spi_read_lm74(int file);
-void read_temperature(void);
+double read_temperature(void);
 
+#endif
