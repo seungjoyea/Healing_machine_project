fadffe39186d3312af4af9184c32c292d3363832
lion42 <74356913+lion42-cat@users.noreply.github.com>
2021-12-16 18:08:36 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <ctype.h>
@@ -9,99 +8,36 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <dirent.h>
-#include <time.h>
-#include <getopt.h>
-#include <errno.h>
-#include <linux/types.h>
-#include <linux/spi/spidev.h>
+#include <pthread.h>
+#include <sys/msg.h>
 
-//사용자 헤더 파일 전처리
-#include "buzzer.h"
-#include "textlcddrv.h"
-#include "fnddrv.h"
-#include "led.h"
-#include "colorled.h"
-#include "temperature.h"
-#include "accelMagGyro.h"
+////////////////////////////////////////
+//////////   메세지 관련    ////////////
+///////////////////////////////////////
 
-//buzzer define
-#define MAX_SCALE_STEP 8
-#define BUZZER_BASE_SYS_PATH "/sys/bus/platform/devices/"
-#define BUZZER_FILENAME "peribuzzer"
-#define BUZZER_ENABLE_NAME "enable"
-#define BUZZER_FREQUENCY_NAME "frequency"
+//message structure
+typedef struct
+{
+long int messageType;
+char bulk_message[1000];
+} structMyMsg;
 
-//testled define
-#define TEXTLCD_DRIVER_NAME		"/dev/peritextlcd"
-
-//fnd define
-#define FND_DRIVER_NAME		"/dev/perifnd"
-
-//led define
-#define LED_DRIVER_NAME		"/dev/periled"
-
-//colorled define
-#define COLOR_LED_DEV_R_ "/sys/class/pwm/pwmchip0/"
-#define COLOR_LED_DEV_G_ "/sys/class/pwm/pwmchip1/"
-#define COLOR_LED_DEV_B_ "/sys/class/pwm/pwmchip2/"
-#define PWM_EXPORT "export"
-#define PWM_UNEXPORT "unexport"
-#define PWM_DUTY "pwm0/duty_cycle"
-#define PWM_PERIOD "pwm0/period"
-#define PWM_ENABLE "pwm0/enable"
-#define PWM_COLOR_R 0
-#define PWM_COLOR_G 1
-#define PWM_COLOR_B 2
-#define PWM_PERIOD_NS 1000000 //ns. = 1ms = 1khz 
+static int cmp1, cmp2, cmp3,cmp4 ,cmp5, cmp6, cmp7, cmp8;  //두 문자열이 같으면 cmp = 0
 
 
-int main(){
+pthread_t tid_led, tid_fnd, tid_textlcd, tid_colorled, tid_temperature, tid_acclMagGyro; //pthread 주소 return
+static int err; //thread 생성 에러 검출
+static int i; //에러확인용 변수
 
-//*****************buzzer program*************//
-    buzzerInit();
-    //buzzerTest();
-    sleep(1);
-    buzzerPlaySong();
-
-//*****************textlcd program*************//
-    int one = 1;
-    int two = 2;
-    char abc[10] = "Hello";
-    char xyz[10] = "embeded";
-
-    lcdtextInit();
-
-    lcdtextWrite(one, abc);
-    lcdtextWrite(two, xyz);
-    sleep(5);
-    
-    lcdtextOff();
-
-//*****************fnd program*************//
-    long x =5;
-    long y =3;
-    long z = 12345;
-    fnd_static_dis(z);
-    sleep(3);
-    fnd_time_dis();
-    sleep(3);
-    fnd_count_dis(x);
-    sleep(3);
-    fnd_rev_count_dis(y);
-    sleep(3);
-    fndOff();
-
-//*****************led program*************//
-    ledLibInit();
-	 test();
+void* led_Func(void *arg)
+{
+	ledLibInit();
+	test();
 	
+	AllLEDon_1s_twoLED_OFF_1s();
+	ledAllBlink_4sec();
 	
-	 AllLEDon_1s_twoLED_OFF_1s();
-	
-	 ledAllBlink_4sec();
-	
-	 while(1){
+	while(1){
     ledOnOff (0x01,0x01);
     sleep(1);
     ledOnOff(0x02,0x02);
@@ -109,56 +45,140 @@ int main(){
     ledOnOff(0x01,0x00);
     sleep(1);
     ledOnOff(0x02,0x00);
-     }
-
+}
 	ledLibExit();
+}
 
-//*****************colorled program*************//
-//---입력한 온도 값이라고 생각---//
-    double a = 100;
-    double b = 70;
-    double c = 50;
-    double d = 30;
-    
-    //---led 초기셋팅---//
-    pwmLedInit();
+void* fnd_Func(void *arg)
+{
+	int mode ;
+	int number,counter;
+	
 
-    //---개별 조명 컨트롤---//
-    ColorLED_White();
+	counter_10();
     sleep(1);
-    ColorLED_Red();
-    sleep(1);
-    ColorLED_Green();
-    sleep(1);
-    ColorLED_Blue();
-    sleep(1);
+	fndDisp(123456, 0);
+	sleep(1);
+	
+	timeDis();
+	sleep(1);
+	fndDisp(123456, 0);
+	sleep(1);
+	fndDisp(234567, 0);
+	sleep(1);	
+	fndDisp(345678, 0);
+	sleep(1);	
+	fndOff();
+}
+
+void* textlcd_Func(void *arg)
+{
+	First_line_write();
+	Second_line_write();
+}
+
+void* colorled_Func(void *arg)
+{
+pwmLedInit();
+
+while(1){
+ ColorLED_White();
+ sleep(1);
+ ColorLED_Red();
+ sleep(1);
+ ColorLED_Green();
+ sleep(1);
+ ColorLED_Blue();
+ sleep(1);
+}
+}
+
+void* temperature_Func(void *arg)
+{
+ read_temperature(); //모니터에 나옴
+}
+
+void* acclMagGyro_Func(void *arg)
+{
+ read_acclAccelerometer();
+ read_Magnetometer();
+ read_Gyroscope();
+}
+
+int main(int argc , char **argv)
+{
+	//버튼 관련
+	structMyMsg messageRxData;
+    int msgID = msgget((key_t)9999, IPC_CREAT|0666);
+    if(msgID == -1){                               //우체통 생성 확인
+        printf("Cannot get msgQueueID\n"); 
+        return -1;
+    }
+
+    while (1) 
+    {
+    int returnValue = 0;
+    returnValue = msgrcv(msgID, &messageRxData, sizeof(messageRxData.bulk_message
+    ), 0, 0); //Wait here if no message
+
+    cmp1 = strcmp(messageRxData.bulk_message, "1");
+    cmp2 = strcmp(messageRxData.bulk_message, "2");
+    cmp3 = strcmp(messageRxData.bulk_message, "3");
+    cmp4 = strcmp(messageRxData.bulk_message, "4");
+    cmp5 = strcmp(messageRxData.bulk_message, "5");
+    cmp6 = strcmp(messageRxData.bulk_message, "6");
+    cmp7 = strcmp(messageRxData.bulk_message, "7");
+    cmp8 = strcmp(messageRxData.bulk_message, "8");
+
+
+    if(cmp1==0) {
+        printf("button1 ");
+        err = pthread_create(&tid_led, NULL, &led_Func, NULL);  //thread_led 생성
+        if(err != 0) printf("Thread Create Error: [%d]\n", i);  //Thread 생성 확인 
+	}
+    else if (cmp2==0){
+        printf("button2 ");
+	    err = pthread_create(&tid_fnd, NULL, &fnd_Func, NULL);  //thread_button 생성
+       if(err != 0) printf("Thread Create Error: [%d]\n", i);  //Thread 생성 확인	
+	}
+    else if (cmp3==0){
+        printf("button3 "); 
+        err = pthread_create(&tid_textlcd, NULL, &textlcd_Func, NULL);  //thread_button 생성
+        if(err != 0) printf("Thread Create Error: [%d]\n", i);  //Thread 생성 확인  
+    }
+    else if (cmp4==0)
+        printf("button4 ");
+    else if (cmp5==0)
+        printf("button5 ");
+    else if (cmp6==0)
+        printf("button6 ");
+    else if (cmp7==0)
+        printf("pressed\n");
+    else if (cmp8==0)
+        printf("released\n");
+	}
+
+
+    /*
     
-    //---온도값에 따른 색상 변화 함수---//
-    pwmLed_thermalcheckmode(a);
-    sleep(2);
-    pwmLed_thermalcheckmode(b);
-    sleep(2);
-    pwmLed_thermalcheckmode(c);
-    sleep(2);
-    pwmLed_thermalcheckmode(d);
-    sleep(2);
+
+
+    err = pthread_create(&tid_colorled, NULL, &colorled_Func, NULL);  //thread_button 생성
+    if(err != 0) printf("Thread Create Error: [%d]\n", i);  //Thread 생성 확인
+
+    err = pthread_create(&tid_temperature, NULL, &temperature_Func, NULL);  //thread_button 생성
+    if(err != 0) printf("Thread Create Error: [%d]\n", i);  //Thread 생성 확인
     
-    //---led끄기 및 종료---//
-    pwmLedOff();
-
-//*****************temperature program*************//
-    double t;
-    t = read_temperature();
-    
-    printf("이것은 read_temperature 함수의 출력값(온도) : %lf \n", t);
-
-//*****************accelMagGyro program*************//
-    p_Accel();
-    p_Magnet();
-    p_Gyro();
+    err = pthread_create(&tid_acclMagGyro, NULL, &acclMagGyro_Func, NULL);  //thread_button 생성
+    if(err != 0) printf("Thread Create Error: [%d]\n", i);  //Thread 생성 확인
 
 
-
-    
-    return 0;
+    pthread_join(tid_led,NULL); //Thread 종료시까지 wait
+    pthread_join(tid_fnd,NULL); //Thread 종료시까지 wait
+    pthread_join(tid_textlcd,NULL); //Thread 종료시까지 wait
+    pthread_join(tid_colorled,NULL); //Thread 종료시까지 wait    
+    pthread_join(tid_temperature,NULL); //Thread 종료시까지 wait    
+    pthread_join(tid_acclMagGyro,NULL); //Thread 종료시까지 wait    
+	return 0;
+	*/
 }
